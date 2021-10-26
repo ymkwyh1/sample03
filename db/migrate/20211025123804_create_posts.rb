6f3df87185057df520d50bab527028f55268f369
Yuhei Yamakawa <ymkwyh1@gmail.com>
2021-10-26 19:54:11 +0900

    PostとUserのリレーション

--- db/migrate/20211025123804_create_posts.rb
+++ db/migrate/20211025123804_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      t.references :user,           null: false
       t.text :content,              null: false, default: ""
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,11 @@
 ActiveRecord::Schema.define(version: 2021_10_25_123804) do
 
   create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
     t.text "content", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
