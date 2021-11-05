67eb993d4743c422b9dc29c13cfa46411f509652
Yuhei Yamakawa <ymkwyh1@gmail.com>
2021-11-05 17:24:51 +0900

    validation test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,27 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
 
-  it '内容が入力されていれば、コメントを保存できる' do
+  context '内容が入力されている' do
 
-    user = User.create!({
-      email: 'test@example.com',
-      password: 'password',
-      username: 'aaaaa'
-    })
+    let!(:user) do
+      user = User.create!({
+        email: 'test@example.com',
+        password: 'password',
+        username: 'aaaaa'
+      })
+    end
+  
+    let!(:post) do
+      post = user.posts.build({
+        picture: Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/heart.png')),
+        content: 'aaaaa'
+      })
+    end
 
-    post = user.posts.build({
-      picture: Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/heart.png')),
-      content: 'aaaaa'
-    })
+    let!(:comment) do
+      comment = post.comments.build({
+        content: 'aaaaa'
+      })
+    end
 
-    comment = post.comments.build({
-      content: 'aaaaa'
-    })
 
-    comment.user_id = user.id
+    it 'コメントを保存できる' do
+ 
+      comment.user_id = user.id
+      expect(comment).to be_valid
+  
+    end
 
-    expect(comment).to be_valid
 
   end
+
   
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,21 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
-  it '写真と内容が入力されていれば、投稿を保存できる' do
+  context '写真と内容が入力されている' do
 
-    user = User.create!({
-      email: 'test@example.com',
-      password: 'password',
-      username: 'aaaaa'
-    })
+    let!(:user) do
+      user = User.create!({
+        email: 'test@example.com',
+        password: 'password',
+        username: 'aaaaa'
+      })
+    end
 
-    post = user.posts.build({
-      picture: Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/heart.png')),
-      content: 'aaaaa'
-    })
+    let!(:post) do
+      post = user.posts.build({
+        picture: Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/heart.png')),
+        content: 'aaaaa'
+      })
+    end
+ 
 
-    expect(post).to be_valid
+    it '投稿を保存できる' do
+  
+      expect(post).to be_valid
+  
+    end
 
   end
-
+  
 end
